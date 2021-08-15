da602832988d1b01d6cf0f09f8a5c3557e1f433b
Rahimjon <rahimmurodovich@gmail.comn>
2021-08-15 14:15:00 +0500

    amiloydagi birinchi gitga ulash

---
+++ script.js
@@ -0,0 +1,47 @@
+"use strict";
+
+const name2 = "rahimjon"; // data type = string
+// console.log(1);
+
+let number = 3; // data-type = number
+// console.log(number);
+
+number = 5;
+// console.log(number);
+
+const pi = 3.14;
+
+const obj = {
+  a: 10,
+};
+
+obj.a = 20;
+
+// console.log(obj);
+
+const person = {
+  name: "Rahimjon",
+  userName: "Abdug'afforov",
+  age: 26,
+};
+
+// console.log(person.name);
+
+// alert("Hello world");
+// const userAge = confirm("siz 18 yoshdan kattamisiz");
+// console.log(userAge);
+// const userName = prompt("Ismingizni kiriting");
+// console.log(userName);
+
+// const answers = [];
+// answers[0] = prompt("ismingiz nima");
+// answers[1] = prompt("Familiyangiz nima");
+// answers[2] = prompt("Yoshingiz nechida");
+// console.log(answers);
+
+// const firstName = prompt("ismingiz nima?");
+// console.log(firstName);
+// const age = +prompt("yoshingiz nechida?");
+// console.log(age);
+
+
